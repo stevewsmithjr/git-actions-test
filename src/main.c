d5545196da82c6729676fbb49b2efb9aadf475b9
stevewsmithjr <stevewsmithjr@gmail.com>
2023-06-20 20:22:11 -0500

    test lint-action

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 void main(int argc, char **argv) {
-
+    int test
     int test_int = 100;
     print_int(test_int);
-    return 0;
     
+    if (1) {
+        printf("the curly brace should be on a new line!\n%d",test_int);
+    }
+
+    return 0;
 }
