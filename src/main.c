8ac45e1d5adf41fc793edc7d1d5adcf5e4f71fc5
stevewsmithjr <stevewsmithjr@gmail.com>
2023-06-20 20:01:36 -0500

    inital commit

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void main(int argc, char **argv) {
+
+    int test_int = 100;
+    print_int(test_int);
+    return 0;
+    
+}
---
+++ src/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+static int print_int(int x);
